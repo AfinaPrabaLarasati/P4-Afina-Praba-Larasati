b5f059da559518e861ae6fee7ab85eb20e580a1d
AfinaPrabaLarasati <64615688+AfinaPrabaLarasati@users.noreply.github.com>
2020-05-15 00:26:41 +0800

    Add files via upload
    Buatlah program array sebagai karakter dengan menampilkan nama setiap Mahasiswa

---
+++ jobsheet 9 C++.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"Nama : Afina Praba Larasati\n";
+cout<<"Nim : F1B019008\n";
+cout<<"Kelompok: Kelompok 1\n\n";
+
+int i;
+char nama[20]={'A','f','i','n','a',' ','p','r','a','b','a',' ','l','a','r','a','s','a','t','i'};
+char nama2[30]="Afina Praba Larasati";
+cout<<"Array per Karakter= ";
+for(i=0;i<=19;i++)
+{
+cout<<nama[i];
+}
+cout<<endl; cout<<"Array string ="<<nama2<<endl<<endl;
+return 0;
+}
+
