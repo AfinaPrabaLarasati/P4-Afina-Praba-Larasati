4cf5af091bb3e23996eb5fbb6c9e47278541ff06
AfinaPrabaLarasati <64615688+AfinaPrabaLarasati@users.noreply.github.com>
2020-05-15 00:24:30 +0800

    Add files via upload
    Buatlah program mengisinilai array dengan perulangan dengan index array dan batas pengulangan menggunakan NIM
    Terakhir setiap mahasiswa.

---
+++ jobsheet 7 C++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+main (){
+cout<<"Nama : Afina Praba Larasati\n";
+cout<<"Nim : F1B019008\n";
+cout<<"Kelompok: Kelompok 1\n\n";
+int nim [8];
+for (int i = 0; i<= 8; i++){
+cout<< "Masukkan nim ke-1 : " << i+1;
+cin>>nim[i];
+}
+}
+
