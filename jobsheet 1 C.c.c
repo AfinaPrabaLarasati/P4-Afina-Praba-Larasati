1e47c049e1714f362365bdc649a4150354ee248a
AfinaPrabaLarasati <64615688+AfinaPrabaLarasati@users.noreply.github.com>
2020-05-14 23:58:37 +0800

    Add files via upload
    Buatlah program array satu dimensi yang menampilkan NIM masing-masing mahasiswa.

---
+++ jobsheet 1 C.c.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main()
+{
+char* nim_ray[8]={"01","02","03","04","05","06","07","08"};
+	printf("nim mahasiswa ke- 1 = %s\n",nim_ray[0]);
+	printf("nim mahasiswa ke- 2 = %s\n",nim_ray[1]);
+	printf("nim mahasiswa ke- 3 = %s\n",nim_ray[2]);
+	printf("nim mahasiswa ke- 4 = %s\n",nim_ray[3]);
+	printf("nim mahasiswa ke- 5 = %s\n",nim_ray[4]);
+	printf("nim mahasiswa ke- 6 = %s\n",nim_ray[5]);
+	printf("nim mahasiswa ke- 7 = %s\n",nim_ray[6]);
+	printf("nim mahasiswa ke- 8 = %s\n",nim_ray[7]);
+
+}
+
