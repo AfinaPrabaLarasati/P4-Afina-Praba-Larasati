2ae86315527675fac1dddb30fcbf3c036b5eb46d
AfinaPrabaLarasati <64615688+AfinaPrabaLarasati@users.noreply.github.com>
2020-05-15 00:23:58 +0800

    Add files via upload
    Buatlah program mengisinilai array dengan perulangan dengan index array dan batas pengulangan menggunakan NIM
    Terakhir setiap mahasiswa.

---
+++ jobsheet 7 C.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main (){
+printf ("Nama : Afina Praba Larasati\n");
+printf ("Nim : F1B019008\n ");
+printf ("Kelompok : 1\n\n");
+
+int nim [8], i;
+for (i = 0; i<= 8; i++){
+printf ("Masukkan Nim ke-%i :", i+1);
+scanf ("%i", &nim[i]);
+}
+}
+
