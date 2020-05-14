947a40dd5c03fb45b6fa353eb9a0e523c38b4f00
AfinaPrabaLarasati <64615688+AfinaPrabaLarasati@users.noreply.github.com>
2020-05-15 00:01:05 +0800

    Add files via upload
    Buatlah program array satu dimensi yang menampilkan NIM masing-masing mahasiswa.

---
+++ jobsheet 1 C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Afina Praba Larasati-F1B019-008\n\n";
+int nim[8]= {8,2,3,1,7,9,8,5};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+	cout << "Nim ke-6 adalah " << nim[5] << endl;
+	cout << "Nim ke-7 adalah " << nim[6] << endl;
+	cout << "Nim ke-8 adalah " << nim[8] << endl;
+}
